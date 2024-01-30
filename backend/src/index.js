7232e18425002c2fc0206a0700285aec8c1d0bff
AnasurRehman <anasurrehman12@gmail.com>
2024-01-30 14:39:44 +0500

    utils changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const port = process.env.PORT || 8001;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => console.error(error));
+
+    app.listen(port, () => {
+      console.log(`App is Listening to ${port}`);
+    });
+  })
+  .catch();
